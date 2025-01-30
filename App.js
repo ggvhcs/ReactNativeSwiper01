b8a928bc981d2b315267ab13307729a1cf5c81c5
ggvhcs <ggvhcs@gmail.com>
2025-01-30 14:16:39 -0500

    january 30, 2025.

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+
 import ReactNativeSwiper from "./components/ReactNativeSwiper";
 
 export default function App() {
@@ -14,8 +15,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
